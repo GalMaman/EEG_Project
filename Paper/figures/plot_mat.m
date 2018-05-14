8d7f61579324f0c4335f5791c97b327180b46193
oryair <oryair36@gmail.com>
2018-05-14 11:48:52 +0300

    Edit Plot File (and git ignore)

--- Paper/figures/plot_mat.m
+++ Paper/figures/plot_mat.m
@@ -1,39 +1,72 @@
-
-cd('C:\Users\User\Desktop\gal\Technion\EEG_Project\Paper\figures\example_rotation_8_subj');
-
-%% Riemannian Geometry
-load data.mat;
-title_str = [];
-
-%% PT
-load data_PT.mat;
-title_str = 'with PT';
-
-%% Rotation
-load data_rot.mat;
-title_str = 'with PT and rotation';
+% 
+% cd('C:\Users\User\Desktop\gal\Technion\EEG_Project\Paper\figures\example_rotation_8_subj');
+% 
+% %% Riemannian Geometry
+% load data.mat;
+% title_str = [];
+% 
+% %% PT
+% load data_PT.mat;
+% title_str = 'with PT';
+% 
+% %% Rotation
+% load data_rot.mat;
+% title_str = 'with PT and rotation';
 
 %%
-% load labels.mat;
-ax = [];
-subj_names = {'C01';'C02';'C03';'C04';'C05';'C06';'C07';'C08';'C10';'C11';'C12';'S01';'S02';'S03';'S04';'S05'};
-color_list = {[0 1 1];[1 0 1];[0 1 0]}; % for 3 stimulations
+close all
+clear
 
-figure(); hold on; ax(1) = gca;
+%%
+RiemannianGeometry    = 1;
+PtGeometry            = 2;
+PtAndRotationGeometry = 3;
+
+subject      = 2;
+GeometryCase = PtAndRotationGeometry;
+
+dirPath = ['./example_rotation_', num2str(subject), '_subj/'];
+switch (GeometryCase)
+    case RiemannianGeometry
+        fileName  = 'Riemannian_Geometry/data.mat';
+        title_str = 'Riemannian_Geometry';
+    case PtGeometry
+        fileName  = 'PT/data_PT.mat';
+        title_str = 'PT';
+    case PtAndRotationGeometry
+        fileName  = 'PT_and_rotation/data_rot.mat';
+        title_str = 'PT and rotation';
+        vAxis     = [-10, 10, -5, 5, -2, 2];
+end
+
+load([dirPath, fileName]);
+load([dirPath, 'labels.mat']);
+    
+%%
+ax         = [];
+subj_names = {'C01';'C02';'C03';'C04';'C05';'C06';'C07';'C08';'C10';'C11';'C12';'S01';'S02';'S03';'S04';'S05'};
+color_list = {[0 1 1];
+              [1 0 1];
+              [0 1 0]}; % for 3 stimulations
+
+figure; hold on; grid on; set(gca, 'FontSize', 16); ax(1) = gca;
 num_sub = unique(full_label_struct{2});
 for ii = 1 : length(num_sub)
     idx = find(full_label_struct{2} == num_sub(ii));
-    scatter3(pca_vec(1,idx), pca_vec(2,idx), pca_vec(3,idx),50,full_label_struct{2}(idx), 'Marker','x','LineWidth',1.5);  
+    scatter3(pca_vec(1,idx), pca_vec(2,idx), pca_vec(3,idx), 100, full_label_struct{2}(idx), 'Marker', 'x', 'LineWidth', 3);  
 end
 subj_str = subj_names(num_sub);
 xlabel('$\psi_1$','Interpreter','latex');
 ylabel('$\psi_2$','Interpreter','latex');
 zlabel('$\psi_3$','Interpreter','latex');
-legend(subj_str(:), 'Interpreter', 'latex', 'location','best');
-title(sprintf('PCA map, colored per subject %s', title_str),'interpreter','latex');
+h = legend(subj_str(:), 'location','best'); set(h, 'Interpreter', 'Latex');
+title(title_str, 'Interpreter', 'Latex');
+axis(vAxis);
 % view(-89.46,11.92);
 
-figure(); hold on; ax(2) = gca;
+%%
+if 0
+figure; hold on; grid on; set(gca, 'FontSize', 16); ax(2) = gca;
 num_stim = unique(full_label_struct{3});
 for ii = 1 : length(num_stim)
     idx = find(full_label_struct{3} == num_stim(ii));
@@ -47,3 +80,4 @@ title(sprintf('PCA map, colored per stimulus %s', title_str),'interpreter','late
 % view(-89.46,11.92);
 
 linkprop(ax,{'CameraPosition','CameraUpVector'}); 
+end
